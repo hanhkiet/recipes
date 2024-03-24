7d68a944dd40007c5844105a46860fd1ecb17554
Huynh Anh Kiet <huynhanhkiet179@gmail.com>
2024-03-24 11:14:24 +0700

    change step in cooking screen and happy_eating_screen.dart

--- lib/ui/screens/cooking/widgets/cooking_step.dart
+++ lib/ui/screens/cooking/widgets/cooking_step.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:recipes/data/recipe_step.dart';
 
-class CookingStep extends StatelessWidget {
+class CookingStep extends StatefulWidget {
   const CookingStep({
     super.key,
     required this.data,
+    required this.isDone,
   });
 
   final RecipeStepData data;
+  final bool isDone;
 
+  @override
+  State<CookingStep> createState() => _CookingStepState();
+}
+
+class _CookingStepState extends State<CookingStep> {
   @override
   Widget build(BuildContext context) {
     return Card(
+      surfaceTintColor: widget.isDone ? Colors.blue : Colors.white,
       child: ListTile(
-        // contentPadding: const EdgeInsets.all(4),
-        title: Text(data.title, style: Theme.of(context).textTheme.bodyLarge),
-        subtitle: Text(data.description,
-            style: Theme.of(context).textTheme.bodyMedium),
-        leading: const CircleAvatar(
-          child: Icon(Icons.check),
+        title: Text(
+          widget.data.title,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        subtitle: Text(
+          widget.data.description,
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+        leading: CircleAvatar(
+          backgroundColor: Colors.blue,
+          child: widget.isDone
+              ? const Icon(
+                  Icons.check,
+                  color: Colors.white,
+                )
+              : null,
         ),
       ),
     );
